4ecd3176e8bfcab0cfab3eeae0e2952c7eb57ac1
cr.tagadiya@gmail.com <cr.tagadiya@gmail.com>
2019-08-30 22:10:38 +0530

    Two pointer Approach in O(N) time complexity

--- easy/Sqaures_Of_Sorted_Array_Problem_977.py
+++ easy/Sqaures_Of_Sorted_Array_Problem_977.py
@@ -12,7 +12,7 @@ Example 2:
 Input: [-7,-3,2,3,11]
 Output: [4,9,9,49,121]
 """
-
+import  collections
 class Solution(object):
     # NlogN Approach
     def sortedSquares(self, A):
@@ -22,3 +22,26 @@ class Solution(object):
         """
 #         O(Nlogn)
         return sorted([item**2 for item in A])
+    # two poiter approach similar to binary search
+    def sortedSquares_using_deque(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        #  O(N) using dequeue
+        answer = collections.deque()
+        l, r = 0, len(A) - 1
+        while l <= r:
+            left, right = abs(A[l]), abs(A[r])
+            if left > right:
+                answer.appendleft(left * left)
+                l += 1
+            else:
+                answer.appendleft(right * right)
+                r -= 1
+        return answer
+
+sol=Solution()
+
+print(sol.sortedSquares_using_deque([-9,-1,0,3,10]))
+print(sol.sortedSquares_using_deque([-7,-3,2,3,11]))
