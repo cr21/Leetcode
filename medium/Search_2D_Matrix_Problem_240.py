9de602db569aeae6a7eb5c6a5183c3201a65d733
cr.tagadiya@gmail.com <cr.tagadiya@gmail.com>
2019-11-28 12:38:11 +0530

    Search_2D matrix done

--- medium/Search_2D_Matrix_Problem_240.py
+++ medium/Search_2D_Matrix_Problem_240.py
@@ -76,3 +76,69 @@ target=100
 print(Sol.searchMatrix(matrix,100))#False
 
 print(Sol.searchMatrix(matrix,24))#True
+
+
+
+"""
+Try this soluion and understand
+https://leetcode.com/problems/search-a-2d-matrix-ii/discuss/396054/What's-the-time-complexity-of-this-approach
+"""
+
+"""
+class Solution:
+    def tighten(self, Y1, Y2, X1, X2):
+        if Y1 == Y2 and X1 == X2:
+            return self.matrix[Y1][X1] == self.target
+
+        t, b = Y1, Y2
+        while t < b:
+            i = (t+b+1) // 2
+            if self.matrix[i][X1] < self.target:
+                t = i
+            elif self.matrix[i][X1] > self.target:
+                b = i-1
+            else:
+                return True
+        y2 = b
+
+        t, b = Y1, Y2
+        while t < b:
+            i = (t+b) // 2
+            if self.matrix[i][X2] < self.target:
+                t = i+1
+            elif self.matrix[i][X2] > self.target:
+                b = i
+            else:
+                return True
+        y1 = b
+
+        l, r = X1, X2
+        while l < r:
+            i = (l+r+1) // 2
+            if self.matrix[Y1][i] < self.target:
+                l = i
+            elif self.matrix[Y1][i] > self.target:
+                r = i-1
+            else:
+                return True
+        x2 = r
+
+        l, r = X1, X2
+        while l < r:
+            i = (l+r) // 2
+            if self.matrix[Y2][i] < self.target:
+                l = i+1
+            elif self.matrix[Y2][i] > self.target:
+                r = i
+            else:
+                return True
+        x1 = r
+
+        return self.tighten(y1, y2, x1, x2)
+    
+    def searchMatrix(self, matrix, target):
+        self.matrix = matrix
+        self.target = target
+        return self.tighten(0, len(matrix)-1, 0, len(matrix[0])-1) if matrix and matrix[0] else False
+
+"""
